b35dcb10d16cc69e850d5943e924a1063bc6c85a
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2014-01-04 20:28:57 +0100

    Adding first GY theme.

---
+++ gomiyama/functions.php
@@ -0,0 +1,55 @@
+<?php
+// File Security Check
+if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename( $_SERVER['SCRIPT_FILENAME'] ) ) {
+    die ( 'You do not have sufficient permissions to access this page!' );
+}
+?>
+<?php
+
+/*-----------------------------------------------------------------------------------*/
+/* Start WooThemes Functions - Please refrain from editing this section */
+/*-----------------------------------------------------------------------------------*/
+
+// WooFramework init
+require_once ( get_template_directory() . '/functions/admin-init.php' );	
+
+/*-----------------------------------------------------------------------------------*/
+/* Load the theme-specific files, with support for overriding via a child theme.
+/*-----------------------------------------------------------------------------------*/
+
+$includes = array(
+				'includes/theme-options.php', 			// Options panel settings and custom settings
+				'includes/theme-functions.php', 		// Custom theme functions
+				'includes/theme-actions.php', 			// Theme actions & user defined hooks
+				'includes/theme-comments.php', 			// Custom comments/pingback loop
+				'includes/theme-js.php', 				// Load JavaScript via wp_enqueue_script
+				'includes/sidebar-init.php', 			// Initialize widgetized areas
+				'includes/theme-widgets.php',			// Theme widgets
+				'includes/theme-install.php',			// Theme installation
+				'includes/theme-woocommerce.php'		// WooCommerce options
+				);
+
+// Allow child themes/plugins to add widgets to be loaded.
+$includes = apply_filters( 'woo_includes', $includes );
+				
+foreach ( $includes as $i ) {
+	locate_template( $i, true );
+}
+
+/*-----------------------------------------------------------------------------------*/
+/* You can add custom functions below */
+/*-----------------------------------------------------------------------------------*/
+
+
+
+
+
+remove_action( 'admin_notices', 'woothemes_updater_notice' );
+
+
+
+
+/*-----------------------------------------------------------------------------------*/
+/* Don't add any code below here or the sky will fall down */
+/*-----------------------------------------------------------------------------------*/
+?>
---
+++ gomiyama/header.php
@@ -0,0 +1,132 @@
+<?php
+// File Security Check
+if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename( $_SERVER['SCRIPT_FILENAME'] ) ) {
+    die ( 'You do not have sufficient permissions to access this page!' );
+}
+?>
+<?php
+/**
+ * Header Template
+ *
+ * Here we setup all logic and XHTML that is required for the header section of all screens.
+ *
+ * @package WooFramework
+ * @subpackage Template
+ */
+ 
+ global $woo_options, $woocommerce;
+ 
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="<?php if ( $woo_options['woo_boxed_layout'] == 'true' ) echo 'boxed'; ?> <?php if (!class_exists('woocommerce')) echo 'woocommerce-deactivated'; ?>">
+<head>
+
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+
+<title><?php woo_title(''); ?></title>
+<?php woo_meta(); ?>
+<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" media="screen" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php
+	wp_head();
+	woo_head();
+?>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-39485584-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</head>
+
+<body <?php body_class(); ?>>
+<?php woo_top(); ?>
+
+<div id="wrapper">
+
+	
+	<div id="top">
+		<nav class="" role="navigation">
+			<?php if ( function_exists( 'has_nav_menu' ) && has_nav_menu( 'top-menu' ) ) { ?>
+			<?php wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'top-nav', 'menu_class' => 'nav fl', 'theme_location' => 'top-menu' ) ); ?>
+			<?php } ?>
+			<?php
+				if ( class_exists( 'woocommerce' ) ) { 
+					echo '<ul class="nav wc-nav">';
+					echo '<li class="cart">'.current(woocommerce_cart_link()).'</li>';
+					echo '<li class="checkout"><a href="'.esc_url($woocommerce->cart->get_checkout_url()).'">'.__('Checkout','woothemes').'</a></li>';
+					echo get_search_form();
+					echo '</ul>';
+				}
+			?>
+		</nav>
+	</div><!-- /#top -->
+
+   
+    
+    <?php woo_header_before(); ?>
+
+	<header id="header" >
+			  
+	   
+	    
+	    <hgroup>
+	    	
+	    	 <?php
+			    $logo = esc_url( get_template_directory_uri() . '/images/logo.png' );
+				if ( isset( $woo_options['woo_logo'] ) && $woo_options['woo_logo'] != '' ) { $logo = $woo_options['woo_logo']; }
+				if ( isset( $woo_options['woo_logo'] ) && $woo_options['woo_logo'] != '' && is_ssl() ) { $logo = preg_replace("/^http:/", "https:", $woo_options['woo_logo']); }
+			?>
+			<?php if ( ! isset( $woo_options['woo_texttitle'] ) || $woo_options['woo_texttitle'] != 'true' ) { ?>
+			    <a id="logo" href="/" title="<?php esc_attr( get_bloginfo( 'description' ) ); ?>">
+			    	<img src="<?php echo $logo; ?>" alt="<?php esc_attr( get_bloginfo( 'name' ) ); ?>" />
+			    </a>
+		    <?php } ?>
+	        
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<h3 class="nav-toggle"><a href="#navigation">&#9776; <span><?php _e('Navigation', 'woothemes'); ?></span></a></h3>
+		      	
+		</hgroup>
+        
+        <?php woo_nav_before(); ?>
+
+		<!--div id="sliderbox">
+		<div id="slideroverlay"></div>
+		</div-->
+
+		<div id="navigation" role="navigation">
+			
+			<?php
+			if ( function_exists( 'has_nav_menu' ) && has_nav_menu( 'primary-menu' ) ) {
+				wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'main-nav', 'menu_class' => 'nav fr', 'theme_location' => 'primary-menu' ) );
+			} else {
+			?>
+	        <ul id="main-nav" class="nav fl">
+				<?php wp_list_pages( 'sort_column=menu_order&depth=6&title_li=&exclude=' ); ?>
+			</ul>
+	        <?php } ?>
+
+			<!--div id="featured-products">
+			<h3>Featured Products</h3>
+			<?php echo do_shortcode("[featured_products per_page='12' columns='4']"); ?>
+			</div-->
+
+	
+		</div>
+		
+		<?php woo_nav_after(); ?>
+
+	
+	</header>
+
+
+	<?php woo_content_before(); ?>
