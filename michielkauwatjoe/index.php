be747f497f551f715c2ff34223fabf362122b293
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2014-01-04 20:30:45 +0100

    First commit for personal theme.

---
+++ michielkauwatjoe/index.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+
+get_header(); ?>
+
+
+	<div id="content" class="narrowcolumn" role="main">
+
+	<?php if (have_posts()) : ?>
+
+		<?php while (have_posts()) : the_post(); ?>
+			<div class="post_before"></div>
+			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+				<small><?php the_time('F jS, Y') ?> <!-- by <?php the_author() ?> --></small>
+
+				<div class="entry">
+					<?php the_content('Read the rest of this entry &raquo;'); ?>
+
+				</div>
+
+				<div class="buttons">
+				<div class="button">
+				<?php echo tweetbutton(); ?>
+				</div>
+				</div>
+
+				<p class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?><?php edit_post_link(' | Edit', ''); ?>  <!--?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?-->
+				<?php show_hide_comments_link(); ?>
+				<?php display_ajax_comments(); ?>
+				</p>
+			</div>
+
+		<?php endwhile; ?>
+
+		<div class="navigation">
+			<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+			<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+		</div>
+
+	<?php else : ?>
+
+		<h2 class="center">Not Found</h2>
+		<p class="center">Sorry, but you are looking for something that isn't here.</p>
+		<?php get_search_form(); ?>
+
+	<?php endif; ?>
+
+	</div>
+
+
+<?php get_footer(); ?>
+
